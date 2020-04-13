a6dbf146ac2e3ccc2e61df42469e7a9525ecedb3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-13 03:24:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,6 +26,8 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
+  "SELECT mood, COUNT(mood) FROM bears
+  "
 end
 
 def selects_bear_that_killed_Tim
